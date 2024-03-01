251e2d69be65becc2014a1f53262e438fe698090
win11 <win11@sanshi.com>
2024-03-01 16:50:58 +0800

    添加注释

--- src/test/java/org/springframework/test/ioc/EventAndEventListenerTest.java
+++ src/test/java/org/springframework/test/ioc/EventAndEventListenerTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.ioc.common.event.CustomEvent;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author derekyi
  * @date 2020/12/5
@@ -12,9 +14,16 @@ public class EventAndEventListenerTest {
 
 	@Test
 	public void testEventListener() throws Exception {
+		// 容器初始化时会创建RefreshedEvent
 		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:event-and-event-listener.xml");
-		applicationContext.publishEvent(new CustomEvent(applicationContext));
 
+		TimeUnit.SECONDS.sleep(3);
+		// 手动发布自定义事件
+		applicationContext.publishEvent(new CustomEvent(applicationContext));
+		TimeUnit.SECONDS.sleep(3);
+		// 设置容器关闭时发布ClosedEvent
 		applicationContext.registerShutdownHook();//或者applicationContext.close()主动关闭容器;
+		TimeUnit.SECONDS.sleep(3);
+
 	}
 }
