aa2c07e735350ab03b2fc8bf8c35bbf0e5301674
win11 <win11@sanshi.com>
2024-03-01 17:08:02 +0800

    添加注释

--- src/test/java/org/springframework/test/aop/PointcutExpressionTest.java
+++ src/test/java/org/springframework/test/aop/PointcutExpressionTest.java
@@ -20,6 +20,10 @@ public class PointcutExpressionTest {
 		Class<HelloService> clazz = HelloService.class;
 		Method method = clazz.getDeclaredMethod("sayHello");
 
+		// 实验效果是，我们定义的切点类能够匹配类，也能匹配方法
+		// 最常用的切点表达式是AspectJ的切点表达式。
+		// 需要匹配类，定义ClassFilter接口；匹配方法，定义MethodMatcher接口。PointCut需要同时匹配类和方法，包含ClassFilter和MethodMatcher，
+		// AspectJExpressionPointcut是支持AspectJ切点表达式的PointCut实现，简单实现仅支持execution函数。
 		assertThat(pointcut.matches(clazz)).isTrue();
 		assertThat(pointcut.matches(method, clazz)).isTrue();
 	}
