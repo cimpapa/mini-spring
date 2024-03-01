0e9d10a42fbcadaf166ee9a96e94f7854e15edce
win11 <win11@sanshi.com>
2024-03-01 09:51:45 +0800

    添加注释

--- src/test/java/org/springframework/test/ioc/BeanDefinitionAndBeanDefinitionRegistryTest.java
+++ src/test/java/org/springframework/test/ioc/BeanDefinitionAndBeanDefinitionRegistryTest.java
@@ -12,10 +12,14 @@ public class BeanDefinitionAndBeanDefinitionRegistryTest {
 
 	@Test
 	public void testBeanFactory() throws Exception {
+		// 创建一个工厂类
 		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		// 定义BeanDefinition并注册
 		BeanDefinition beanDefinition = new BeanDefinition(HelloService.class);
 		beanFactory.registerBeanDefinition("helloService", beanDefinition);
-
+		// 1. 去检查singletonMap里是否有该bean
+		// 2. 如果有直接返回
+		// 3.如果没有，通过beanDefinitionMap获取beanDefinition，通过beanDefinition的Class反射创建并加入到singletonMap
 		HelloService helloService = (HelloService) beanFactory.getBean("helloService");
 		helloService.sayHello();
 	}
